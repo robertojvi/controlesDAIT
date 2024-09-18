05056bcc6929427d1b3939db3c12f9bff5036a90
rpinos <rpinos@etapa.net.ec>
2024-09-18 16:33:49 -0500

    Se agrega estrucutura SpringBoot de proyecto

---
+++ controlDait/controlDait/src/main/java/com/controlDait/ControlDaitApplication.java
@@ -0,0 +1,16 @@
+package com.controlDait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlDaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlDaitApplication.class, args);
+	}
+
+}
+
+
+
---
+++ controlDait/controlDait/src/test/java/com/controlDait/ControlDaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlDait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlDaitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
